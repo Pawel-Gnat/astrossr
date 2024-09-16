988ae28d22b1999f26d51f8285cdb51a37c26a24
Pawel Gnat <pawel_gnat@o2.pl>
2024-09-16 21:00:23 +0200

    build: remove node

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,8 @@ import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import netlify from '@astrojs/netlify'
 
-import node from '@astrojs/node'
-
 export default defineConfig({
 	integrations: [mdx(), sitemap()],
 	output: 'server',
-	adapter: netlify({
-		edgeMiddleware: true,
-	}),
+	adapter: netlify(),
 })
