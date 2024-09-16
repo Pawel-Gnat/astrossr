3bbd7c2c7655c0e12d2eafce714bc90e014d0b1b
Pawel Gnat <pawel_gnat@o2.pl>
2024-09-16 19:36:51 +0200

    fix: add netlify import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
+import netlify from '@astrojs/netlify'
 
 import node from '@astrojs/node'
 
