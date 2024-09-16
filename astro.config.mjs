f9fc6c4169ea90fef6ecfb1f8754e8fdaf6b02ba
Pawel Gnat <pawel_gnat@o2.pl>
2024-09-16 19:30:00 +0200

    initial files

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+
+import node from '@astrojs/node'
+
+export default defineConfig({
+	site: 'https://astrotest.pl',
+	integrations: [mdx(), sitemap()],
+	output: 'server',
+	adapter: netlify(),
+})
---
+++ src/i18n/ui.ts
@@ -0,0 +1,27 @@
+export const languages = {
+	en: 'English',
+	pl: 'Polish',
+}
+
+export const defaultLang = 'pl'
+
+export const ui = {
+	en: {
+		'nav.home': 'Home',
+		'nav.blog': 'Blog',
+		'nav.about': 'About',
+	},
+	pl: {
+		'nav.home': 'Strona główna',
+		'nav.blog': 'Blog',
+		'nav.about': 'O mnie',
+	},
+} as const
+
+export const showDefaultLang = false
+
+export const routes = {
+	en: {
+		omnie: 'about',
+	},
+}
