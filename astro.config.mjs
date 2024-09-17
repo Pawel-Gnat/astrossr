664585f2732b6ab40426609ffa9bc4db166e2bb4
Pawel Gnat <pawel_gnat@o2.pl>
2024-09-17 22:05:25 +0200

    build: bump netlify version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import netlify from '@astrojs/netlify/functions'
+import netlify from '@astrojs/netlify'
 
 export default defineConfig({
 	integrations: [mdx(), sitemap()],
 	site: 'https://main--astrossrtest.netlify.app/',
 	output: 'server',
-	adapter: netlify({
-		edgeMiddleware: true,
-	}),
+	adapter: netlify(),
 })
